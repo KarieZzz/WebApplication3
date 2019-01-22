7c421f4793e3cf467aa687ed425bfdfbd8d2774c
KarieZzz <35432904+KarieZzz@users.noreply.github.com>
2019-01-22 22:22:49 +0300

    Initial commit

---
+++ WebApplication3/Controllers/CrudController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CrudController : Controller
+    {
+        private BugTrackerContext db = new BugTrackerContext();
+
+        // GET: Crud
+        public ActionResult Index()
+        {
+            var tickets = db.Tickets.Include(t => t.Category).Include(t => t.Status).Include(t => t.User);
+            return View(tickets.ToList());
+        }
+
+        // GET: Crud/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: Crud/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Title");
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Title");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "FirstName");
+            return View();
+        }
+
+        // POST: Crud/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TicketId,Title,Description,Date,CategoryId,StatusId,UserId")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Title", ticket.CategoryId);
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Title", ticket.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "FirstName", ticket.UserId);
+            return View(ticket);
+        }
+
+        // GET: Crud/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Title", ticket.CategoryId);
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Title", ticket.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "FirstName", ticket.UserId);
+            return View(ticket);
+        }
+
+        // POST: Crud/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TicketId,Title,Description,Date,CategoryId,StatusId,UserId")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Title", ticket.CategoryId);
+            ViewBag.StatusId = new SelectList(db.Statuses, "StatusId", "Title", ticket.StatusId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "FirstName", ticket.UserId);
+            return View(ticket);
+        }
+
+        // GET: Crud/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Crud/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Tickets.Find(id);
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication3/Models/BugTrackerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication3.Models
+{
+    public class BugTrackerContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
